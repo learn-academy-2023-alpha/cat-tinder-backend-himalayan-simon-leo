7692af559857717fd6528104e08f11a32dfe55e0
Melaza6 <cueroleo6@gmail.com>
2023-04-04 12:03:10 -0700

    all model specs in separate test and passing

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,11 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  it "should validate all inputs for adding a cat" do
-    cat = Cat.create
+  it "should validate the name input for adding a cat" do
+    cat = Cat.create(
+      age: 80,
+      enjoys: "hunting Jerry",
+      image: "https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_1280.png"
+    )
     expect(cat.errors[:name]).to_not be_empty
+  end
+  it "should validate the age input for adding a cat" do
+    cat = Cat.create(
+      name: "Tom",
+      enjoys: "hunting Jerry",
+      image: "https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_1280.png"
+    )
     expect(cat.errors[:age]).to_not be_empty
+  end
+  it "should validate the enjoys input for adding a cat" do
+    cat = Cat.create(
+      name: "Tom",
+      age: 80,
+      image: "https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_1280.png"
+    )
     expect(cat.errors[:enjoys]).to_not be_empty
+  end
+  it "should validate the image input for adding a cat" do
+    cat = Cat.create(
+      name: "Tom",
+      age: 80,
+      enjoys: "hunting Jerry",
+    )
     expect(cat.errors[:image]).to_not be_empty
   end
   it "validates enjoys has a minimun length of 10 characters" do
@@ -16,7 +41,6 @@ RSpec.describe Cat, type: :model do
       image: "https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_1280.png"
     )
     expect(cat.errors[:enjoys]).to_not be_empty
-
   end
   
 end
